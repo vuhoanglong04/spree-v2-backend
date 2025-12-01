6bf44b3cba10a32cc493d51083dc22d5e7b95941
Vu Linh Hoang Long <longvulinhhoang@gmail.com>
2025-12-01 11:07:04 +0700

    Preparing view file for testing ELB

--- app/controllers/host_ip_controller.rb
+++ app/controllers/host_ip_controller.rb
@@ -1,6 +1,4 @@
 class HostIpController < ApplicationController
-  def host_ip
-    ip = Socket.ip_address_list.detect(&:ipv4_private?)&.ip_address
-    render plain: "Host IP: #{ip || 'unknown'}"
+  def index
   end
 end
